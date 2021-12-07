577dc4f223f258765e9846fbae2d23dc7632375a
Peterson-moura <72897949+Peterson-moura@users.noreply.github.com>
2021-12-07 19:50:44 -0300

    TQS_Inicial

---
+++ src/main/java/br/com/Moura/TQS_TI18/TqsTi18Application.java
@@ -0,0 +1,13 @@
+package br.com.Moura.TQS_TI18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqsTi18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqsTi18Application.class, args);
+	}
+
+}
